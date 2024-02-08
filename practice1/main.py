888abc607ca514733a64e6701400594e59cdab57
aleck <sansepiol26@gmail.com>
2024-02-07 18:58:46 -0600

    just commeting the code

--- practice1/main.py
+++ practice1/main.py
@@ -1,15 +1,15 @@
-# Using the MVC patter desing 
+"""
+The main file for running the main program.
+"""
 
 import sys
 from controller import Controller
 
 def main():
+    """ main: This function emulates the main c program function """
     controller = Controller()
     controller.mainloop()
     return 0
 
 if __name__ == "__main__":
     sys.exit(main())
-    
-
-
