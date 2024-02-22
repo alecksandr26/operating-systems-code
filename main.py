95c10ee859bd767c9f5ee3e0b7f273fef8ab0935
aleck <sansepiol26@gmail.com>
2024-02-22 14:48:01 -0600

    uploading a main.py version

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+The main file for running the main program.
+"""
+import sys
+from src.controller import Controller
+
+def main() -> int:
+    """
+    main: This function emulates the main c program function
+    It will initialized the controller and run the other things
+    """
+    controller = Controller()
+    controller.mainloop()
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
